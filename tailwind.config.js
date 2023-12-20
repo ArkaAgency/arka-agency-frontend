adb43fab01cf47e086e6805419d11bb332106ff1
AlexandreLG2022 <a.legal.contact@gmail.com>
2023-12-20 23:52:00 +0100

    :package: Installed material tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+const withMT = require('@material-tailwind/react/utils/withMT');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = withMT({
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+})
+
